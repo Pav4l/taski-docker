c537517501ba0acbd717881001a1037311c4dc14
Павел Горев <pavel.gorev@gmail.com>
2023-06-21 13:29:08 +0300

    taski_docker_v2.5

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.69.154', '127.0.0.1', 'localhost', 'pashtetproject.hopto.org']
 
 
 # Application definition
